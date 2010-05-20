749eaa5e6169f3b0602eb7761388f75df594bf7a
Cameron McDonald <hatchpj@gmail.com>
2010-05-20 01:07:00 -0500

    basic map and scrolling support

--- Ninja Jam/main.lua
+++ Ninja Jam/main.lua
@@ -2,6 +2,75 @@ SCREEN_WIDTH  = 640
 SCREEN_HEIGHT = 480
 require("lib/AnAL.lua")
 
+tile = {}
+for i=0,2 do -- change 3 to the number of tile images minus 1.
+	tile[i] = love.graphics.newImage( "images/tile"..i..".gif" )
+end
+
+map = {
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,},
+	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,},
+	{2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,0,1,2,2,2,2,2,2,2,2,},
+}
+
+map_w = #map[1] -- Obtains the width of the first row of the map
+map_h = #map -- Obtains the height of the map
+map_x = 0
+map_y = 0
+map_display_buffer = 2 -- We have to buffer one tile before and behind our viewpoint.
+                       -- Otherwise, the tiles will just pop into view, and we don't want that.
+tile_w = 16
+tile_h = 16
+map_display_w = SCREEN_WIDTH  / tile_w
+map_display_h = SCREEN_HEIGHT / tile_h
+
+function draw_map()
+	offset_x = map_x % tile_w
+	offset_y = map_y % tile_h
+	firstTile_x = math.floor(map_x / tile_w)
+	firstTile_y = math.floor(map_y / tile_h)
+	
+	for y=1, (map_display_h + map_display_buffer) do
+		for x=1, (map_display_w + map_display_buffer) do
+			-- Note that this condition block allows us to go beyond the edge of the map.
+			if y + firstTile_y >= 1 and y + firstTile_y <= map_h and
+			   x + firstTile_x >= 1 and x + firstTile_x <= map_w then
+				love.graphics.draw(
+					tile[map[y + firstTile_y][x + firstTile_x]],
+					((x * tile_w) - offset_x - tile_w),
+					(y * tile_h)  - offset_y - tile_h
+				)
+			end
+		end
+	end
+end
+
 objects = {}
 
 background = { }
@@ -40,7 +109,7 @@ player = { }
 grav = 200
 
 function love.load()
-	world = love.physics.newWorld(SCREEN_WIDTH, SCREEN_HEIGHT)
+	world = love.physics.newWorld(map_w * tile_w, map_h * tile_h)
 	world:setGravity(0, 700)
 	world:setMeter(64)
 	
@@ -69,6 +138,8 @@ function love.update(dt)
 	
 	player.move.x, player.move.y = bodies[1]:getLinearVelocity()
 	player.x, player.y = bodies[1]:getPosition()
+	
+	map_x = (player.x/2) - map_w
 	
 	if player.move.x < 10 and player.move.x > -10 then
 		if player.image == player.left_anim then
@@ -107,6 +178,22 @@ function love.update(dt)
 		end
 		player.image = player.left_anim
 	end
+	
+	if map_x < 0 then
+		map_x = 0
+	end
+	
+	if map_y < 0 then
+		map_y = 0
+	end
+	
+	if map_x > map_w * tile_w - map_display_w * tile_w then
+		map_x = map_w * tile_w - map_display_w * tile_w
+	end
+	
+	if map_y > map_h * tile_h - map_display_h * tile_h then
+		map_y = map_h * tile_h - map_display_h * tile_h
+	end
 end
 
 function love.keypressed(k)
@@ -116,8 +203,9 @@ function love.keypressed(k)
 end
 
 function love.draw()
-	love.graphics.draw(background.image, background.x, background.y)
+	draw_map()
 	
+	--[[
 	local x1, y1, x2, y2, x3, y3, x4, y4 = shapes[0]:getBoundingBox()
 	local boxwidth = x3 - x2
 	local boxheight = y2 - y1
@@ -131,14 +219,15 @@ function love.draw()
 	love.graphics.rectangle("fill", bodies[2]:getX()  - boxbwidth/2, bodies[2]:getY() - boxbheight/2, boxbwidth, boxbheight)
 	
 	love.graphics.setColor(255, 255, 255)
+	--]]
 	
 	player.image:draw(bodies[1]:getX(), bodies[1]:getY(), 0, 1, 1, 9.5, 13)
 	
 	printx, printy = bodies[1]:getLinearVelocity()
 	love.graphics.print(printx, 20, 40)
 	love.graphics.print(printy, 20, 55)
-	love.graphics.print(ground.y, 20, 70)
-	love.graphics.print(player.y + player.image:getHeight()/2, 20, 85)
+	love.graphics.print(player.x, 20, 70)
+	love.graphics.print(map_x, 20, 85)
 	love.graphics.print(player.image:getWidth(), 20, 100)
 	love.graphics.print(player.image:getHeight(), 20, 115)
 end
